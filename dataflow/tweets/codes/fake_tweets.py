377d22ab7769ca1f1d788dbd2f109173e1420e34
varshneynilesh <84116474+varshneynilesh@users.noreply.github.com>
2024-06-30 23:48:34 -0700

    initial

--- dataflow/tweets/codes/fake_tweets.py
+++ dataflow/tweets/codes/fake_tweets.py
@@ -7,9 +7,32 @@ from faker import Faker
 from google.cloud import pubsub
 
 PROJECT_ID = '<PROJECT_ID>'
-TOPIC = 'tweet_topic'
+TOPIC = 'tweets_topic'
 
-username = []
+usernames = []
 faker = Faker()
 publisher = pubsub.PublisherClient()
-topic_path = publisher.topic
+topic_path = publisher.topic_path(PROJECT_ID, TOPIC)
+
+
+def publish(publisher, topic, message):
+    data = message.encode('utf-8')
+    return publisher.publish(topic_path, data=data)
+
+
+def generate_tweet():
+    data = {}
+    data['created_at'] = datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S')
+    data['tweet_id'] = faker.uuid4()
+    data['text'] = faker.sentence()
+    data['user'] = random.choice(usernames)
+    return json.dumps(data)
+
+if __name__ == '__main__':
+    for i in range(200):
+        newprofile = faker.simple_profile()
+        usernames.append(newprofile['username'])
+    print("Hit CTRL-C to stop Tweeting!")
+    while True:
+        publish(publisher, topic_path, generate_tweet())
+        time.sleep(0.5)
