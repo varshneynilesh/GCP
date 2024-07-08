bd3938e66b7411e286cad3c834fec7a4173ababc
varshneynilesh <84116474+varshneynilesh@users.noreply.github.com>
2024-07-07 18:39:33 -0700

    initial

---
+++ dataflow/traffic/run_minute-traffic-pipeline.sh
@@ -0,0 +1,11 @@
+cd ~/GCP/dataflow/traffic
+
+export PROJECT_ID=$(gcloud config get-value project)
+export REGION="us-west1"
+export BUCKET=gs://${PROJECT_ID}
+export PIPELINE_FOLDER=${BUCKET}
+export RUNNER=DataflowRunner
+export INPUT_PATH=${PIPELINE_FOLDER}/events.json
+export TABLE_NAME=${PROJECT_ID}:logs.user_traffic
+
+python3 src/minute-traffic-pipeline.py --project=${PROJECT_ID} --region=${REGION} --staging_location=${PIPELINE_FOLDER}/staging --temp_location=${PIPELINE_FOLDER}/temp --runner=${RUNNER} --input_path=${INPUT_PATH} --table_name=${TABLE_NAME}
