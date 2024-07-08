826c864915ab5b7ee6ebe40e78e8deebb8769efa
varshneynilesh <84116474+varshneynilesh@users.noreply.github.com>
2024-07-07 19:23:09 -0700

    initial

--- dataflow/traffic/run_minute-traffic-pipeline.sh
+++ dataflow/traffic/run_minute-traffic-pipeline.sh
@@ -6,6 +6,6 @@ export BUCKET=gs://${PROJECT_ID}
 export PIPELINE_FOLDER=${BUCKET}
 export RUNNER=DataflowRunner
 export INPUT_PATH=${PIPELINE_FOLDER}/events.json
-export TABLE_NAME=${PROJECT_ID}:logs.user_traffic
+export TABLE_NAME=${PROJECT_ID}:logs.minute_traffic
 
 python3 src/minute-traffic-pipeline.py --project=${PROJECT_ID} --region=${REGION} --staging_location=${PIPELINE_FOLDER}/staging --temp_location=${PIPELINE_FOLDER}/temp --runner=${RUNNER} --input_path=${INPUT_PATH} --table_name=${TABLE_NAME}
