91a150b6e7996162366075f2f9b88055aa7929a0
varshneynilesh <84116474+varshneynilesh@users.noreply.github.com>
2024-07-08 17:58:49 -0700

    Update pipeline.py

--- dataflow/branching/src/pipeline.py
+++ dataflow/branching/src/pipeline.py
@@ -22,9 +22,11 @@ def run():
     parser.add_argument('--project',required=True, help='Specify Google Cloud project')
     parser.add_argument('--region', required=True, help='Specify Google Cloud region')
     parser.add_argument('--runner', required=True, help='Specify Apache Beam Runner')
-    parser.add_argument('--inputPath', required=True, help='Path to events.json')
-    parser.add_argument('--outputPath', required=True, help='Path to coldline storage bucket')
-    parser.add_argument('--tableName', required=True, help='BigQuery table name')
+    parser.add_argument('--staging_location', required=True, help='Specify Cloud Storage bucket for staging')
+    parser.add_argument('--temp_location', required=True, help='Specify Cloud Storage bucket for temp')
+    parser.add_argument('--input_path', required=True, help='Path to events.json')
+    parser.add_argument('--output_path', required=True, help='Path to coldline storage bucket')
+    parser.add_argument('--table_name', required=True, help='BigQuery table name')
 
     opts, pipeline_opts = parser.parse_known_args()
 
@@ -32,12 +34,14 @@ def run():
     options = PipelineOptions(pipeline_opts)
     options.view_as(GoogleCloudOptions).project = opts.project
     options.view_as(GoogleCloudOptions).region = opts.region
-    options.view_as(GoogleCloudOptions).job_name = '{0}{1}'.format('my-pipeline-',time.time_ns())
+    options.view_as(GoogleCloudOptions).staging_location = opts.staging_location
+    options.view_as(GoogleCloudOptions).temp_location = opts.temp_location
+    options.view_as(GoogleCloudOptions).job_name = '{0}{1}'.format('branching-pipeline-',time.time_ns())
     options.view_as(StandardOptions).runner = opts.runner
 
-    input_path = opts.inputPath
-    output_path = opts.outputPath
-    table_name = opts.tableName
+    input_path = opts.input_path
+    output_path = opts.output_path
+    table_name = opts.table_name
 
     # Table schema for BigQuery
     table_schema = {
